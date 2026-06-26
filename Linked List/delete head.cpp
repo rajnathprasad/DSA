be5003438cc986c8a733a97d9cf01f43e1fb9259
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-06-26 19:45:11 +0530

    deletion of head

---
+++ Linked List/delete head.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int data1, Node* next1){
+        data=data1;
+        next=next1;
+    }
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+
+Node* convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i]);
+        mover->next = temp;
+        mover=temp;
+    }
+    return head;
+}
+
+int main(){
+    vector<int> arr={2,5,8,7};
+    int present=0;
+    Node* head = convertArr2LL(arr);
+
+    cout<<"Before Deletion\n";
+    Node* temp=head;
+    while(temp){
+        cout<<temp->data;
+        temp=temp->next;
+    }
+
+    temp = head;
+    head=head->next;
+    free(temp);
+
+    cout<<"\nAfter Deletion\n";
+    temp=head;
+    while(temp){
+        cout<<temp->data;
+        temp=temp->next;
+    }
+    
+
+    return 0;
+}
