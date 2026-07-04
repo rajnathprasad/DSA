f6b4593e36da745eefa98df00b421d6b85c15eb4
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-07-04 16:13:51 +0530

    insert before head DLL

---
+++ Linked List/insert before head DLL.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+    Node(int data1, Node* next1, Node* back1){
+        data=data1;
+        next=next1;
+        back=back1;
+    }
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+        back=nullptr;
+    }
+};
+
+Node* convertArr2DLL(vector<int> &arr){
+    if(arr.empty()) return nullptr;
+    Node* head = new Node(arr[0]);
+    Node* prev= head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i],nullptr,prev);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+}
+
+int countSize(Node* head){
+    int count=0;
+    while(head!=nullptr){
+        count++;
+        head=head->next;
+    }
+    return count;
+}
+
+void print(Node* head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+}
+
+int main(){
+    vector<int> arr={1,2,3};
+    Node* head = convertArr2DLL(arr);
+
+    cout<<"Before Insertion\n";
+    print(head);
+
+    // Logic
+    int k=9;
+    Node* temp = new Node(k);
+
+    if(head != nullptr){
+        temp->next = head;
+        head->back = temp;
+    }
+
+    head = temp;
+
+    //Logic End
+
+    cout<<"\nAfter Insertion\n";
+    print(head);
+    
+
+    return 0;
+}
