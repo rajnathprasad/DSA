7fe9f94f42c854c0e5ce0ede988e602413761d3d
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-07-04 16:34:46 +0530

    insert before tail DLL

---
+++ Linked List/insert before tail DLL.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+    Node(int data1, Node* next1, Node* back1){
+        data=data1;
+        next=next1;
+        back=back1;
+    }
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+        back=nullptr;
+    }
+};
+
+Node* convertArr2DLL(vector<int> &arr){
+    if(arr.empty()) return nullptr;
+    Node* head = new Node(arr[0]);
+    Node* prev= head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i],nullptr,prev);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+}
+
+int countSize(Node* head){
+    int count=0;
+    while(head!=nullptr){
+        count++;
+        head=head->next;
+    }
+    return count;
+}
+
+void print(Node* head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+}
+
+int main(){
+    vector<int> arr={1,2,3};
+    Node* head = convertArr2DLL(arr);
+
+    cout<<"Before Insertion\n";
+    print(head);
+
+    // Logic
+    int k = 9;
+
+    if (head == nullptr) {
+        head = new Node(k);
+    }
+    else if (head->next == nullptr) {
+        Node* insertNode = new Node(k, head, nullptr);
+        head->back = insertNode;
+        head = insertNode;
+    }  
+    else{
+
+
+    Node* temp = head;
+
+    while(temp->next!=nullptr){
+        temp=temp->next;
+    }
+    Node* prev= temp->back;
+    Node* front = temp;
+    Node* insertNode=new Node(k,front,prev);
+    prev->next=insertNode;
+    temp->back=insertNode;
+
+}
+    //Logic End
+
+    cout<<"\nAfter Insertion\n";
+    print(head);
+    
+
+    return 0;
+}
