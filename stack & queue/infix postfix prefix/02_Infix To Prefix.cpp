1bc978398e435576b5a6a0ef15ae868c5c374e40
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-07-18 17:19:12 +0530

    infix to prefix

---
+++ stack & queue/infix postfix prefix/02_Infix To Prefix.cpp
@@ -0,0 +1,52 @@
+class Solution {
+  public:
+    int priority(char c){
+        if(c=='+' || c=='-') return 1;
+        if(c=='*' || c=='/') return 2;
+        if(c=='^') return 3;
+        return -1;
+    }
+    string infixToPrefix(string &s) {
+        // code here
+        string ans;
+        stack <char> st;
+        reverse(s.begin(),s.end());
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(') s[i]=')';
+            else if(s[i]==')') s[i]='(';
+            
+            if((s[i]>='A' && s[i]<='Z') ||
+            (s[i]>='a' && s[i]<='z') ||
+            s[i]>='0' && s[i]<='9'){
+                ans.push_back(s[i]);
+            }
+            
+            else if(s[i]=='('){
+                st.push(s[i]);
+            }
+            else if(s[i]==')'){
+                while(st.top()!='('){
+                    ans.push_back(st.top());
+                    st.pop();
+                }
+                st.pop();
+            }
+            else{
+                while(!st.empty() && st.top()!='(' &&
+                    (priority(st.top()) > priority(s[i]) ||
+                    priority(st.top())==priority(s[i]) && s[i]=='^'))
+                    {
+                        ans.push_back(st.top());
+                        st.pop();
+                    }
+                    st.push(s[i]);
+            }
+        }
+        while(!st.empty()){
+                ans.push_back(st.top());
+                st.pop();
+            }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
