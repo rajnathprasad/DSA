85f17c2526167d056c95ec1e01846260bb80e8ab
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-07-20 17:25:12 +0530

    next smaller element

---
+++ stack & queue/monotonic stack queue/Next Smaller Element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    vector<int> nextSmallerEle(vector<int>& arr) {
+        //  code here
+        int n=arr.size();
+        vector<int> nse(n,0);
+        stack<int> st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && st.top()>=arr[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                nse[i]=-1;
+            }else{
+                nse[i]=st.top();
+            }
+            st.push(arr[i]);
+        }
+        return nse;
+    }
+};
