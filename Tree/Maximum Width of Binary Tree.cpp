0cd5d09ffbd5e6873eea02ed01ad25622ba6b1bb
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-07-27 14:26:11 +0530

    max width of binary tree

--- Tree/Maximum Width of Binary Tree.cpp
+++ Tree/Maximum Width of Binary Tree.cpp
@@ -1,38 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
         queue<pair<TreeNode*,long long>> q;
-        long long ans=0;
-        int maxi=0;
+        unsigned long long maxWidth=0;
         q.push({root,0});
         while(!q.empty()){
+            unsigned long long l=q.front().second;
+            unsigned long long r=q.back().second;
+            maxWidth = max(maxWidth,r-l+1);
             int n=q.size();
-            long long minIndex=q.front().second;
-            long long first, last;
-            for(int i=0;i<n;i++){
-                auto [node, idx] = q.front();
+            while(n--){
+                TreeNode* curr=q.front().first;
+                unsigned long long idx=q.front().second;
                 q.pop();
-                idx-=minIndex;
-                if (i==0) first=idx;
-                if (i==n-1) last=idx;
-                if (node->left)
-                    q.push({node->left,2*idx+1});
-                if (node->right)
-                    q.push({node->right,2*idx+2});
+                if(curr->left) q.push({curr->left,2*idx+1});
+                if(curr->right) q.push({curr->right,2*idx+2});
             }
-            ans=max(ans,last-first+1);
         }
-        return (int)ans;
+        return (int)maxWidth;
     }
 };
