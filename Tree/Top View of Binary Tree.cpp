1aa15a516d711ad2f1cb89fefb27c9fcc3c54541
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-07-28 22:10:34 +0530

    Top View of Binary Tree

---
+++ Tree/Top View of Binary Tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    vector<int> topView(Node *root) {
+        // code here
+        queue<pair<Node*, int>> q;
+        map<int, int> mpp;
+        vector<int> ans;
+        if(root==nullptr) return ans;
+        q.push({root,0});
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            Node* node=it.first;
+            int line=it.second;
+            if(mpp.find(line)==mpp.end()) mpp[line]=node->data;
+            if(node->left) q.push({node->left,line-1});
+            if(node->right) q.push({node->right,line+1});
+        }
+        for(auto it:mpp) ans.push_back(it.second);
+        return ans;
+    }
+};
