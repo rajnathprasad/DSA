8763a7f63e9d62fdbb975db853a3c15fbc5e2ea6
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-08-02 17:42:43 +0530

    Construct Binary Tree from Inorder and Postorder Traversal

---
+++ Tree/Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        map<int, int> mpp;
+        for(int i=0;i<inorder.size();i++){
+            mpp[inorder[i]]=i;
+        }
+        TreeNode* root=buildTree(postorder,0,postorder.size()-1, inorder,0,inorder.size()-1,mpp);
+        return root;
+    }
+    TreeNode* buildTree(vector<int> &postorder, int postStart, int postEnd, vector<int> &inorder, int inStart, int inEnd, map<int, int> &mpp){
+        if(postStart>postEnd || inStart>inEnd) return nullptr;
+        TreeNode* root=new TreeNode(postorder[postEnd]);
+        int inRoot=mpp[postorder[postEnd]];
+        int numsLeft=inRoot-inStart;
+        root->left=buildTree(postorder,postStart,postStart+numsLeft-1, inorder,inStart,inRoot-1,mpp);
+        root->right=buildTree(postorder,postStart+numsLeft,postEnd-1, inorder,inRoot+1,inEnd,mpp);
+        return root;
+    }
+};
