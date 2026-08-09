7e57abf4c1325698ddc5e0eea11754345cd92995
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-08-10 02:38:58 +0530

    BFS of graph

---
+++ Graph/BFS of graph.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // code here
+        vector<int> bfs;
+        queue<int>q;
+        int n=adj.size();
+        int vis[n]={0};
+        vis[0]=1;
+        q.push(0);
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            bfs.push_back(node);
+            for(auto it:adj[node]){
+                if(!vis[it]){
+                    vis[it]=1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+};
