897c5e1383acb068c7bb424498abad3eecd3d297
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-08-11 14:46:53 +0530

    Morris Preorder Traversal

---
+++ Tree/Morris Preorder Traversal.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder;
+        TreeNode* curr=root;
+        while(curr!=nullptr){
+            if(curr->left==nullptr){
+                preorder.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode* prev=curr->left;
+                while(prev->right && prev->right!=curr){
+                    prev=prev->right;
+                }
+                if(prev->right!=curr){
+                    prev->right=curr;
+                    preorder.push_back(curr->val);
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=nullptr;
+                    curr=curr->right;
+                }
+            }
+        }
+        return preorder;
+    }
+};
