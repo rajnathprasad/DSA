8978230ff86eb1bd7b856c16a77b2313a6f1a942
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-09-20 02:07:11 +0530

    dfs of graph

---
+++ Graph/DFS of Graph.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+  
+    void dfs(int node, vector<vector<int>> &adj, vector<int> &vis, vector<int> &ls){
+        vis[node]=1;
+        ls.push_back(node);
+        
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                dfs(it, adj, vis, ls);
+            }
+        }
+    }
+    vector<int> dfs(vector<vector<int>>& adj) {
+        // Code here
+        int n=adj.size();
+        vector<int> vis(n,0);
+        
+        int start=0;
+        
+        vector<int> ls;
+        dfs(start, adj, vis, ls);
+        return ls;
+    }
+};
