095a981f3edb8ae0dbed870ba762a1287eac13c2
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-09-22 18:22:56 +0530

    number of islands

---
+++ Graph/Number of Islands.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>> &grid,vector<vector<int>> &vis,int i,int j){
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() ||
+        vis[i][j] == 1 || grid[i][j] == '0') return;
+
+        vis[i][j]=1;
+
+        dfs(grid, vis, i+1, j);
+        dfs(grid, vis, i, j+1);
+        dfs(grid, vis, i-1, j);
+        dfs(grid, vis, i, j-1);
+
+
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+
+        vector<vector<int>> vis(m, vector<int> (n,0));
+        
+        int count=0;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(vis[i][j]==0 && grid[i][j]=='1'){
+                    count++;
+                    dfs(grid, vis, i,j);
+                }
+            }
+        }
+        return count;
+    }
+};
